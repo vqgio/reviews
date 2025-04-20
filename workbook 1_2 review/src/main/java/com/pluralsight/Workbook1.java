b0abc537be241c0a18a55906d752b2ac6f12d348
Gio <giovannizavala82@gmail.com>
2025-04-19 21:42:24 -0500

    initial commit

--- workbook 1_2 review/src/main/java/com/pluralsight/Workbook1.java
+++ workbook 1_2 review/src/main/java/com/pluralsight/Workbook1.java
@@ -1,5 +1,9 @@
 package com.pluralsight;
 
 public class Workbook1 {
+    public static void main(String[] args) {
+        double number = 5.14;;
 
+        System.out.println(Math.round(number));
+    }
 }
